e77b2bcec710ac5473ed240d78ac05b3371875d0
yonghwankim-dev <yonghwankim.dev@gmail.com>
2023-02-15 16:10:50 +0900

    add: README.md 추가

--- functions/test.kt
+++ functions/test.kt
@@ -10,6 +10,7 @@ fun feedTheFish() {
     val day = randomDay()
     val food = fishFood(day)
     println ("Today is $day and the fish eat $food")
+    println("Change water : ${shouldChangeWater(day)}")
 }
 
 fun randomDay(): String {
@@ -30,5 +31,26 @@ fun fishFood(day: String): String {
         else -> "nothing"
     }
 }
+
+// default value
+fun swim(speed: String = "fast") {
+    println("swimming $speed")
+}
+
+fun shouldChangeWater (day: String, temperature: Int = 22, dirty: Int = 20): Boolean {
+    return when {
+        isTooHot(temperature) -> true
+        isDirty(dirty) -> true
+        isSunday(day) -> true
+        else -> false
+    }
+}
+
+// compact functions 만들기
+fun isTooHot(temperature: Int) = temperature > 30
+
+fun isDirty(dirty: Int) = dirty > 30
+
+fun isSunday(day: String) = day == "Sunday"
 
 
